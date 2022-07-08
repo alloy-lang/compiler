d991f1e17a2fd7752c51d54d699e29c9ad15b807
theochupp <tclchiam@gmail.com>
2022-07-08 18:45:00 -0400

    comments

--- compiler/syntax/src/parser/sink.rs
+++ compiler/syntax/src/parser/sink.rs
@@ -41,7 +41,7 @@ impl<'l, 'input> Sink<'l, 'input> {
                 Event::FinishNode => self.builder.finish_node(),
             }
 
-            self.eat_whitespace();
+            self.eat_trivia();
         }
 
         self.builder.finish()
@@ -52,9 +52,9 @@ impl<'l, 'input> Sink<'l, 'input> {
         self.cursor += 1;
     }
 
-    fn eat_whitespace(&mut self) {
+    fn eat_trivia(&mut self) {
         while let Some(lexeme) = self.lexemes.get(self.cursor) {
-            if lexeme.kind != SyntaxKind::Whitespace {
+            if !lexeme.kind.is_trivia() {
                 break;
             }
 
