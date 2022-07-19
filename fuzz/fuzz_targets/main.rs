b99f668719dc75db0b7166ee76ca73b72f57e1e8
theochupp <tclchiam@gmail.com>
2022-07-19 00:05:55 -0400

    feat: fuzzing with more syntax

--- fuzz/fuzz_targets/main.rs
+++ fuzz/fuzz_targets/main.rs
@@ -5,7 +5,9 @@ use libfuzzer_sys::fuzz_target;
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
         let parse = alloy_rowan_parser::parse(s);
-        let root = alloy_rowan_ast::Root::cast(parse.syntax()).unwrap();
+        let syntax = parse.syntax();
+        let _validation_errors = alloy_rowan_ast::validation::validate(&syntax);
+        let root = alloy_rowan_ast::Root::cast(syntax).unwrap();
         let (_database, _stmts) = alloy_rowan_hir::lower(root);
     }
 });
