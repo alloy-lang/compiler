f479318a6f89780350ea61cca663dc3f62a396f4
theochupp <tclchiam@gmail.com>
2023-10-07 01:47:53 -0400

    refactor: ast::Module has separate functions for each type it contains

--- compiler/ast/src/ast/module.rs
+++ compiler/ast/src/ast/module.rs
@@ -1,20 +1,31 @@
 #[allow(clippy::wildcard_imports)]
 use super::*;
 
-ast_node!(ModuleDef, fields: [definitions]);
+ast_node!(ModuleDef, fields: [imports, traits, behaviors, type_definitions, type_annotations, values]);
 
 impl ModuleDef {
     #[must_use]
-    pub fn definitions(&self) -> Vec<ModuleDefinition> {
+    pub fn imports(&self) -> Vec<ImportDef> {
+        children(self)
+    }
+    #[must_use]
+    pub fn traits(&self) -> Vec<TraitDef> {
+        children(self)
+    }
+    #[must_use]
+    pub fn behaviors(&self) -> Vec<BehaviorDef> {
+        children(self)
+    }
+    #[must_use]
+    pub fn type_definitions(&self) -> Vec<TypeDefinition> {
+        children(self)
+    }
+    #[must_use]
+    pub fn type_annotations(&self) -> Vec<TypeAnnotation> {
+        children(self)
+    }
+    #[must_use]
+    pub fn values(&self) -> Vec<ValueDef> {
         children(self)
     }
 }
-
-ast_union_node!(ModuleDefinition, kinds: [
-    ImportDef,
-    TraitDef,
-    BehaviorDef,
-    TypeDefinition,
-    TypeAnnotation,
-    ValueDef
-]);
